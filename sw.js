27189f0b10899538299df344c4db0a0b65423316
tlafitte2727 <152379100+tlafitte2727@users.noreply.github.com>
2026-09-25 15:47:16 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE="checklist-bali-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icons/icon-180.png","./icons/icon-192.png","./icons/icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
-    const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return resp;
-  }).catch(()=>caches.match("./index.html"))));
-});
